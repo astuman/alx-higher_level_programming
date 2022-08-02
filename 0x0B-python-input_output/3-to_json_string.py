ec2e114924815603f3d1ed9fe7ad0038200f341c
Astewul <loloalex1992@gmail.com>
2022-08-02 16:31:09 -0300

    commiting

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # 3-to_json_string.py
 """Defining a function that reprsents JSON"""
+
+
 import json
 def to_json_string(my_obj):
     """Return the JSOn representaion of a string object"""
