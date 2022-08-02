09ba878425c5105c22933dcec364c65382d57bb2
Astewul <loloalex1992@gmail.com>
2022-08-02 12:49:13 -0300

    commiting

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #1-write_file.py
-"""Defining a file writing function"""
+"""Defining a file-writing function"""
 
 
 def write_file(filename="", text=""):
@@ -9,7 +9,8 @@ def write_file(filename="", text=""):
        filename (str): the name of text to be write
        text (str): the text to write to the file
        Returns :
-           the number of charachters written"""
+           the number of charachters written
+           """
     
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
