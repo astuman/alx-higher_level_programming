ab0c7e5521b4fe197bb97c58e6deb082c8947c30
Astewul <loloalex1992@gmail.com>
2022-09-06 16:51:04 -0300

    CREAT TABLE

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,3 @@
 -- create new table
 -- create table IF NOT EXISTS first_table COLUMNS(id int, name varchar(256));
-USE mysql;
 CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
