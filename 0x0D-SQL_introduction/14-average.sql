43716071a5c098ecb113cfe75f4b1648de0bdf82
Astewul <loloalex1992@gmail.com>
2022-09-06 18:09:48 -0300

    Select average score

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
---Computes the average score and outputs as average
-SELECT avg(score) AS average FROM second_table;
+-- Computes the average score and outputs as average
+SELECT AVG(score) AS average FROM second_table;
