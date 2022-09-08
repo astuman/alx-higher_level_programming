04250c602e5d3403be89017ff52ef7b2da5d9cea
Astewul <loloalex1992@gmail.com>
2022-09-08 10:08:28 -0300

    Create table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,3 @@
 -- Create table force_name
-CREATE TABLE IF NOT EXISTS `force_name` (`ID` INT, `name` VARCHAR(256) NOT NULL);
+use mysql;
+CREATE TABLE IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
