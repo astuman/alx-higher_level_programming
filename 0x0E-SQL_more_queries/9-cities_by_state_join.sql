1ac038e4889a78ac53682d32349b93c17f454cb8
Astewul <loloalex1992@gmail.com>
2022-09-08 15:38:00 -0300

    task select cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- LISTS ALL CITIES CONTAINE IN 'hbtn_0d_usa'
-SELECT c.`id` c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON  c.`state_id` = s.`id` ORDER BY c.`id`;
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON  c.`state_id` = s.`id` ORDER BY c.`id`;
