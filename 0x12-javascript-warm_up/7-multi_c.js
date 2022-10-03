bcbc407c650d1ff474cd0a86a06a7a099aed0140
Astewul <Astewul>
2022-10-03 15:22:01 +0300

    print x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 // prints C is fun 'x' times
+const lang = 'C is fun';
 const x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i <= parseInt(x); i++) {
-    console.log(process.argv[i]);
+    console.log(lang);
   }
 }
