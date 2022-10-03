13963b9bb86e84b5b5f110dcfac9e7b4999fdf31
Astewul <Astewul>
2022-10-03 14:19:04 +0300

    convert value to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,6 +3,6 @@
 
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
-  } else {
-console.log('My number: ' + parseInt(process.argv[2]));
-  }
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
