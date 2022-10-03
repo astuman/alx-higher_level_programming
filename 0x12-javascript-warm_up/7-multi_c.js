0329531cbdcbe4f8f9024c5189240d6cf69f4a71
Astewul <Astewul>
2022-10-03 15:29:47 +0300

    Print 3 lins

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-// prints 3 lines but by using loop
+// prints C is fun 'x' times by using loop
 
 const lang = 'C is fun';
-
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i <= parseInt(process.argv[2]); i++) {
-    console.log(lang);
+    console.log(lang);;
   }
 }
