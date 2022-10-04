e28bef05441cc5aa4a5655ecbb2b375a15e89b35
Astewul <Astewul>
2022-10-04 17:51:55 +0300

    Add two argv

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
 function add (a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 }
-console.log(add(a, b));
+console.log(add(process.argv[2], process.argv[3]));
