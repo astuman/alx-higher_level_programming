0327c2f210500b110410d85892cebd3b89c24db5
Astewul <Astewul>
2022-10-04 17:33:27 +0300

    Print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('X'.repeat(parseInt(process.argv[2])));
+    console.log('x'.repeat(parseInt(process.argv[2])));
   }
 }
