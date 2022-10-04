485690cecb15dd57030bb6d2c73ff1e7e60992a7
Astewul <Astewul>
2022-10-04 17:46:25 +0300

    add two numbers from arg

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/user/bin/node
+// print addition of two integers
+
+
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+
+function add (a, b) {
+	return a + b;
+}
+console.log(add(a, b))
