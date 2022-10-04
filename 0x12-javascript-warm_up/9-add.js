9d54309065392d991983472cef096f1b5f74101a
Astewul <Astewul>
2022-10-04 17:53:01 +0300

    Add two argv

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,6 @@
 #!/user/bin/node
 // print addition of two integers
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
 function add (a, b) {
   return parseInt(a) + parseInt(b);
 }
