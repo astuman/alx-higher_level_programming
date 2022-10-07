77f884e259753e83df109dcd100d4158ddd3435d
Astewul <Astewul>
2022-10-07 20:09:25 +0300

    filet data by user input

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     myCursor = db.cursor()
-    myCursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    myCursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY \
+    id ASC")
     rows = myCursor.fetchall()
     for row in rows:
         if row[1][0] == 'N':
