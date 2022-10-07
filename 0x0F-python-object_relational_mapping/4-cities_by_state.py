d19879531553a05feb975eb55f940257171c4687
Astewul <Astewul>
2022-10-07 23:00:02 +0300

    filter by cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     mycursor = db.cursor()
-    mycursor.execute("SELECT * FORM states WHERE cities LIKE %s ORDER BY \
-            id ASC", (argv[4]),)
+    mycursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
     rows = mycursor.fetchall()
     for row in rows:
         print(row)
