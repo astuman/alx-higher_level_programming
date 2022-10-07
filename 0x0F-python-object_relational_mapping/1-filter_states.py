8eb16865996e0876307ce926955460e667024550
Astewul <Astewul>
2022-10-07 19:05:55 +0300

    select by filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+from email import charset
 import MySQLdb
 
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     myCursor = db.cursor()
     myCursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = myCursor.fetchall()
