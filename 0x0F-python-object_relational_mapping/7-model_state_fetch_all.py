b6c2a0e146ed7b97297aeb445a7c57af6705f241
Astewul <Astewul>
2022-10-09 20:05:46 +0300

    fetch all state via ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 """
-model state ORM
+lists all State objects from a database
 """
 
 import sqlalchemy
@@ -10,13 +9,13 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-
 if __name__ == "__main__":
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                        argv[2], argv[3]))
+                                                                    argv[2],
+                                                                    argv[3]))
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
     for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(State.id, State.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
