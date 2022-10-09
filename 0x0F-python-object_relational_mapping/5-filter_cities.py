ef93ef8fd573fbe8d97f0c4c1c80e50a046c1a6a
Astewul <Astewul>
2022-10-09 13:49:33 +0300

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+print cities using parametr state name
+"""
+import MySQLdb
+
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    mycursor = db.cursor()
+    mycursor.execute("SELECT name FROM states WHERE cities LIKE %s ORDEDR BY \
+                     id ASC", (argv[4]),)
+    rows = mycursor.fetchall()
+    for row in rows:
+        print(row)
+    mycursor.close()
+    db.close()
