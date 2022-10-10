b5122f7054a3acb71223f482c8dd647b57bb81ae
Astewul <Astewul>
2022-10-10 22:27:22 +0300

    print first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    eng = create_engine("mysql+mysql://{}:{}@localhost/{}".format(argv[1],
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
                                                                   argv[2],
                                                                   argv[3]))
     Base.metadata.create_all(eng)
