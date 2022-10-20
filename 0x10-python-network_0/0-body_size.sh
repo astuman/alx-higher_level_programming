bc29db2272a43fc1b89ad6ba3f040ab6636fa41b
Astewul <loloalex1992@gmail.com>
2022-10-20 19:26:17 +0300

    commit task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # it take a URL sends a request to that URL and display the size of the body
-curl -sI "$1" | grep 'Content-length:' | cut -d ' ' -f2
+curl -sI "$1" | grep 'Content-length:' | cut -d' ' -f2
