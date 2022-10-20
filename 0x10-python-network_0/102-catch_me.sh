ef391276a473f3063ca1539096863cc9fab50f63
Astewul <loloalex1992@gmail.com>
2022-10-20 23:10:36 +0300

    advanced task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -sLX PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "Origin: You got me!" 0.0.0.0:5000/catch_me
