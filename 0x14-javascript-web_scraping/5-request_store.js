e796c50c0ba18680d60703989a7e50174359af42
Astewul <loloalex1992@gmail.com>
2022-11-16 11:01:17 +0300

    get content

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(path, body, 'utf-8');
+  }
+});
